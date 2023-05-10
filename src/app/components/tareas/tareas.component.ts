162a4fe53b87959c0f71bd3c57520956c3b02757
arie.karlen <ariel.karlen@possumus.tech>
2023-05-10 11:11:31 -0300

    Clase 15 | Listado de tareas

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tarea } from 'src/app/models/tarea';
 
 @Component({
   selector: 'app-tareas',
   templateUrl: './tareas.component.html',
   styleUrls: ['./tareas.component.css']
 })
-export class TareasComponent {
+export class TareasComponent implements OnInit {
 
+listaTareas: tarea[]=[];
+nombreTarea:string='';
+
+constructor() {}
+ngOnInit(): void {
+  
+}
+
+AgregarTarea() {
+  const tarea: tarea = {
+    nombre: this.nombreTarea,
+    estado:false
+  }
+
+  this.listaTareas.push(tarea);
+
+  this.nombreTarea='';
+}
 }
